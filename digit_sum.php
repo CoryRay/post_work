340a2e9b3fcb8bd7a477e5c353e36a79ce6662a5
Cory Rodriguez <coreyreylp@gmail.com>
2014-11-11 13:00:31 -0600

    started digit sum post work

---
+++ digit_sum.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sum up Those Digits</title>
+</head>
+<body>
+    <form action="" method='GET'>
+        <label for="number">Input:</label>
+        <input type="number" name='number'>
+    </form>
+</body>
+</html>
